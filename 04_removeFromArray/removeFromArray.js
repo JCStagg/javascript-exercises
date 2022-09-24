8482f46956bf1a3b70de8953356373ca42ce3ef2
Jacob Stagg <JCStagg+Github@gmail.com>
2022-09-24 00:19:50 -0500

    Implement function to filter elements from an array according to spec.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
+let testArray = [1, 2, 3, 4, 5, 6];
 
+const removeFromArray = function(array) {
+    let output = [];
+    for (item in array) {
+        let removeTest = '';
+        for (argument in arguments) {
+            if (parseInt(argument)===0) {
+                continue;
+            };
+            if (arguments[argument] === array[item]) {
+                removeTest = 'Fail';
+                break;
+            };
+        };
+        if (removeTest != 'Fail') {
+            output.push(array[item]);
+        };
+    };
+    return output;
 };
 
 // Do not edit below this line
