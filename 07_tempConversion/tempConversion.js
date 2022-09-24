d44556ec933eab5774e39168fdb2e9804641b5e5
Jacob Stagg <JCStagg+Github@gmail.com>
2022-09-24 01:07:05 -0500

    Implement function to convert Fahrenheit to Celsius and another function to do the opposite according to spec.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,12 @@
-const ftoc = function() {
-
+const ftoc = function(fahrTemp) {
+  return Math.round(((fahrTemp-32) * (5/9)) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(celsiusTemp) {
+  return Math.round(((celsiusTemp * (9/5)) + 32)*10) / 10;
 };
+
+console.log(ftoc(32));
 
 // Do not edit below this line
 module.exports = {
