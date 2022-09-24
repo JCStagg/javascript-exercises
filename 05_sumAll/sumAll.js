26060c40dd0862ea441c5ecece6a60f02f0eebd7
Jacob Stagg <JCStagg+Github@gmail.com>
2022-09-24 00:38:47 -0500

    Implement function to sum numbers between and including the parameters according to spec.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
+    if ((typeof(firstNumber) != typeof(1)) || typeof(secondNumber) != typeof(1)) {
+        return 'ERROR';
+    } else if (firstNumber < 0 || secondNumber < 0) {
+        return 'ERROR';
+    };
 
+    let lowNumber = firstNumber;
+    let highNumber = secondNumber;
+
+    if (firstNumber < secondNumber) {
+        lowNumber = firstNumber;
+        highNumber = secondNumber;
+    } else if (firstNumber > secondNumber) {
+        lowNumber = secondNumber;
+        highNumber = firstNumber;
+    }
+    
+    let sum = 0;
+
+    for (let i = lowNumber; i <=highNumber; i++) {
+        sum += i;
+    };
+    return sum;
 };
 
 // Do not edit below this line
